1ffb87839073b9576e09ab2347f5fc10120f308a
Zufar-M <dondini.zver@gmail.com>
2025-07-23 21:17:03 +0300

    refactor: BookController
    - Deleted test method for db connection
    - Changed fieldPersonDAO to BookDAO

--- src/main/java/io/github/zufarm/library/controllers/BookController.java
+++ src/main/java/io/github/zufarm/library/controllers/BookController.java
@@ -1,25 +1,22 @@
 package io.github.zufarm.library.controllers;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import io.github.zufarm.library.dao.PersonDAO;
+import io.github.zufarm.library.dao.BookDAO;
 
 @Controller
 @RequestMapping("/books")
 public class BookController {
-	private final PersonDAO personDAO;
+	private final BookDAO bookDAO;
 	
 	@Autowired
-	public BookController(PersonDAO personDAO) {
-		this.personDAO = personDAO;
+	public BookController(BookDAO bookDAO) {
+		this.bookDAO = bookDAO;
 	}
 	
 	@GetMapping()
 	public String showBooks() {
-        personDAO.testConnection();
         return "books/all";
     }
 }
