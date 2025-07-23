e9ed7c58804649e5e6e415921b12da558015e4ae
Zufar-M <dondini.zver@gmail.com>
2025-07-23 21:49:00 +0300

    feat: add showAll method+view to PersonController
    - Add controller method to get all people
    - Add view template to display all people

--- src/main/java/io/github/zufarm/library/controllers/PersonController.java
+++ src/main/java/io/github/zufarm/library/controllers/PersonController.java
@@ -1,16 +1,29 @@
 package io.github.zufarm.library.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import io.github.zufarm.library.dao.PersonDAO;
 
 @Controller
 @RequestMapping("/people")
 public class PersonController {
 	
+	private final PersonDAO personDAO;
+	
+	@Autowired
+	public PersonController(PersonDAO personDAO) {
+		this.personDAO = personDAO;
+	}
+
 	@GetMapping()
-	public String showPeople() {
-        
+	public String showPeople(Model model) {
+		model.addAttribute("people", personDAO.showAll());
         return "people/all";
     }
+	
+	
 }
