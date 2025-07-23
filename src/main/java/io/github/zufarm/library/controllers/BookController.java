61e04f97f2fbc712cb8fd0911358b4b99704d0ce
Zufar-M <dondini.zver@gmail.com>
2025-07-24 02:52:40 +0300

    feat: implement book-person relationship logic
    - Added 2 new BookController methods for assigning book to person and releasing it from person
    
    - Added personId field in Book class to track relation between person and book
    
    - Changed BookDAO update logic to consider new person_id field from Book class
    
    - Added new logic to view book/one to propose different options depending on book status (busy/free)

--- src/main/java/io/github/zufarm/library/controllers/BookController.java
+++ src/main/java/io/github/zufarm/library/controllers/BookController.java
@@ -10,18 +10,23 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 import io.github.zufarm.library.dao.BookDAO;
+import io.github.zufarm.library.dao.PersonDAO;
 import io.github.zufarm.library.models.Book;
+
 
 @Controller
 @RequestMapping("/books")
 public class BookController {
 	private final BookDAO bookDAO;
+	private final PersonDAO personDAO;
 	
 	@Autowired
-	public BookController(BookDAO bookDAO) {
+	public BookController(BookDAO bookDAO, PersonDAO personDAO) {
 		this.bookDAO = bookDAO;
+		this.personDAO = personDAO;
 	}
 	
 	@GetMapping()
@@ -43,14 +48,15 @@ public class BookController {
 	
 	@GetMapping("/{id}")
     public String showOne(@PathVariable("id") int id, Model model) {
-		Book book = bookDAO.showOne(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Person with this id not found"));
+		Book book = bookDAO.showOne(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Book with this id not found"));
         model.addAttribute("book", book);
+        model.addAttribute("people", personDAO.showAll());
         return "books/one";
     }
 	
 	@GetMapping("/{id}/edit")
     public String edit(Model model, @PathVariable("id") int id) {
-		Book book = bookDAO.showOne(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Person with this id not found"));
+		Book book = bookDAO.showOne(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Book with this id not found"));
         model.addAttribute("book", book);
         return "books/edit";
     }
@@ -67,4 +73,20 @@ public class BookController {
         return "redirect:/books";
     }
 	
+	@PostMapping("/{id}/assign")
+	public String assign(@PathVariable("id") int bookId, @RequestParam("personId") int personId) {
+		Book book = bookDAO.showOne(bookId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Book with this id not found"));
+		book.setPersonId(personId);
+		bookDAO.update(bookId, book);
+        return "redirect:/books/" + bookId;
+        
+    }
+	@PostMapping("/{id}/release")
+	public String release(@PathVariable("id") int bookId) {
+		Book book = bookDAO.showOne(bookId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Book with this id not found"));
+		book.setPersonId(null);
+		bookDAO.update(bookId, book);
+        return "redirect:/books/" + bookId;
+        
+    }
 }
