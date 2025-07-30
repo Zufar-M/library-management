e817bb599439b1465f19f4a72356332ede5a1e77
Zufar-M <dondini.zver@gmail.com>
2025-07-30 03:45:18 +0300

    feat: migration to default AuthProvider
    - Changed project structure and config
    - Deleted custom AuthProvider
    - Project now uses default Spring Security DAO Auth Provider, does the same as custom Auth Provider before
    - Deleted log code in BookController

--- src/main/java/io/github/zufarm/library/config/SecurityConfig.java
+++ src/main/java/io/github/zufarm/library/config/SecurityConfig.java
@@ -1,30 +1,32 @@
 package io.github.zufarm.library.config;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import io.github.zufarm.library.services.AppUserDetailsService;
 
-import io.github.zufarm.library.security.AuthProvider;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-private final AuthProvider authProvider;
-	
+private final AppUserDetailsService appUserDetailsService;
 	
 	@Autowired
-    public SecurityConfig(AuthProvider authProvider) {
-		this.authProvider = authProvider;
+	public SecurityConfig(AppUserDetailsService appUserDetailsService) {
+		this.appUserDetailsService = appUserDetailsService;
 	}
 
 	@Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-        	.authenticationProvider(authProvider)
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/login").permitAll()
                 .anyRequest().authenticated()           
@@ -35,5 +37,16 @@ private final AuthProvider authProvider;
             );
 
         return http.build();
+    }
+	
+	@Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+	
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance(); 
     }
 }
